861508e081f417efd59a2f5fca68b95699abeebb
SkyPW <161071809+SkyPW@users.noreply.github.com>
2024-05-01 11:11:56 -0500

    Update ViewController.swift

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -2,9 +2,7 @@
 //  ViewController.swift
 //  TravelBook
 //
-//  Created by Atil Samancioglu on 27.07.2019.
-//  Copyright © 2019 Atil Samancioglu. All rights reserved.
-//
+//Created by  on 4/19/24.
 
 import UIKit
 import MapKit
